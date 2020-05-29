b98b15443e795e432daac745531afd068c1d6801
우인희 <ihwoo@MacBookAir-IH.local>
2020-05-29 23:16:08 +0900

    Updates

---
+++ precache-manifest.4b2605aca0aab2c4dea2aeb3d5c4dd94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1101798000d043213fbdbbe221c281f",
+    "url": "/movie2020/index.html"
+  },
+  {
+    "revision": "92416b9646f2c6957e5a",
+    "url": "/movie2020/static/css/main.7123ca20.chunk.css"
+  },
+  {
+    "revision": "0facc7b862f19ac8b0e6",
+    "url": "/movie2020/static/js/2.f8426c0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie2020/static/js/2.f8426c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92416b9646f2c6957e5a",
+    "url": "/movie2020/static/js/main.45cf0e51.chunk.js"
+  },
+  {
+    "revision": "e2ed6a6911c0ea8ce61e",
+    "url": "/movie2020/static/js/runtime-main.353261eb.js"
+  }
+]);
